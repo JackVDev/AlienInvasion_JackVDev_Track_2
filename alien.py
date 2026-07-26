e2ec24e7b2de8f9ab39b3b56401c5f5713494826
Jack Verdin <jackverdin@icloud.com>
2026-07-26 16:53:39 -0400

    alien.py: Added docstring to the Class and Methods

--- alien.py
+++ alien.py
@@ -12,7 +12,19 @@ if TYPE_CHECKING:
     from alien_fleet import AlienFleet
 
 class Alien(Sprite):
+    """A class handling an individual alien
+
+    Args:
+        Sprite (pygame Sprite): Details about the sprite
+    """
     def __init__(self, fleet: 'AlienFleet', x: float, y: float):
+        """Initializes the class
+
+        Args:
+            fleet (AlienFleet): A reference back to the fleet that holds the Alien
+            x (float): The x location of the alien
+            y (float): The y location of the alien
+        """
         super().__init__()
         self.fleet = fleet
         self.screen = fleet.game.screen
@@ -30,6 +42,8 @@ class Alien(Sprite):
         self.y = float(self.rect.y)
 
     def update(self):
+        """Moves the alien based on settings.fleet_speed and fleet_direction
+        """
         temp_speed = self.settings.fleet_speed
 
         self.x += temp_speed * self.fleet.fleet_direction
@@ -37,7 +51,14 @@ class Alien(Sprite):
         self.rect.y = self.y
 
     def check_edges(self):
+        """Checks if the alien is colliding with the edges of the screen
+
+        Returns:
+            bool: True if the alien is colliding with the edges, False if not
+        """
         return (self.rect.right >= self.boundaries.right or self.rect.left <= self.boundaries.left)
     
     def draw_alien(self):
+        """Draws the alien to the screen
+        """
         self.screen.blit(self.image, self.rect)
